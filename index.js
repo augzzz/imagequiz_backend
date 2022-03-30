8f3e19edc3d36820b50029ebdfa5feef8c30bf74
augzzz <amoore124@email.arizona.edu>
2022-03-29 19:35:31 -0700

    1st commit.

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { store } = require('./temp-store/store');
+
+const application = express();
+const port = 4002;
+
+// middleware
+application.use(express.json());
+//
+
+application.get('/', (request, response) => {
+    response.status(200).json( { done: true, message: 'All good.' } );
+})
+
+application.post('/register', (request, response) => {
+    let name = request.body.name;
+    let email = request.body.email;
+    let password = request.body.password;
+
+    store.addCustomer(name, email, password);
+    response.status(200).json( { done: true, message: 'Customer added successfully.' } );
+})
+
+application.post('/login', (request, response) => {
+    let email = request.body.email;
+    let password = request.body.password;
+    let result = store.login(email, password);
+    if (result.valid) {
+        response.status(200).json( {done: true, message: 'Customer logged in successfully.'} );
+    } else {
+        response.status(401).json( {done: false, message: result.message} );
+    }
+})
+
+application.get('/quiz/:id', (request, response) => {
+    let id = request.params.id;
+    let result = store.getQuiz(id);
+    if (result.done) {
+        response.status(200).json( {done: true, result: result.quiz} );
+    } else {
+        response.status(404).json( {done: false, message: result.message} );
+    }
+})
+
+application.listen(port, () => {
+    console.log(`Listening to port ${port} `);
+})
---
+++ temp-store/store.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrpyt');
+const { hash } = require('bcrypt');
+let { customers } = require('./customers');
+let { quizzes } = require('./data');
+
+let store = {
+
+    addCustomer: (name, email, password) => {
+        const hash = bcrypt.hashSync(password, 10);
+        customers.push( {id: 1, name: name, email: email, password: hash} );
+
+    }, 
+
+    login: (email, password) => {
+        let customer = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
+        if (customer) {
+            let valid = bcrypt.compareSync(password, customer.password);
+            if (valid) {
+                return { valid: true };
+            } else {
+                return { valid: false, message: 'Credentials are not valid.' }
+            }
+        } else {
+            return { valid: false, message: 'Email not found.' }
+        }
+    },
+
+    getQuiz: (id) => {
+        let quiz = quizzes.find( x = x.name.toLowerCase() === id.toLowerCase() );
+        if (quiz) {
+            return { done: true, quiz };
+        } else {
+            return { done: false, message: 'No quiz this name was not found.' };
+        }
+    }
+
+}
+
+module.exports = { store }; 
