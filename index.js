c0edefccf20c97aed63abe9cd59d829ad37c91b6
augzzz <amoore124@email.arizona.edu>
2022-04-30 21:22:03 -0700

    debugging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4002;
 
 // MIDDLEWARE //
 application.use(cors({
-    origin: "https://augzzz.github.io/imagequiz/",
+    origin: "https://augzzz.github.io/ISTA330/practicum/week-10/index.html",
     credentials: true
 }));
 
